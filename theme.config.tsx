018be72ebd9aa527f649d3c8f242ba767122a13d
Mohammadhossein Fakhraei <121129466+Linuxmaster14@users.noreply.github.com>
2025-09-05 10:58:41 +0330

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>MagicWP Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://docs.magicwp.io',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://magicwp.io',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
